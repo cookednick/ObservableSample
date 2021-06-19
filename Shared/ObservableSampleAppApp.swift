7a525168236a6082e0cecdb239fd931d0abd94d3
Nick Cook <nick@considerate.software>
2021-06-19 09:52:44 -0700

    Initial Commit

---
+++ Shared/ObservableSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableSampleAppApp.swift
+//  Shared
+//
+//  Created by Nick on 6/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ObservableSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
